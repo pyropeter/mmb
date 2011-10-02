98cb7d4a8be001e92f79fa1f710828f3eff4f7d7
PyroPeter <abi1789@googlemail.com>
2011-10-02 19:24:47 +0200

    Unsigned ints for coordinates, part II

---
+++ src/defs.h
@@ -0,0 +1,12 @@
+#ifndef _MMB_DEFS_H
+#define _MMB_DEFS_H
+
+#include <sys/types.h>
+
+#define MAXCOMP 4294967295L
+#define HALFCOMP 2147483647L
+typedef u_int32_t Comp;
+
+typedef char Block;
+
+#endif /* _MMB_DEFS_H */
