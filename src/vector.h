82c5e44ac858d460d985f31e5ace2d9f5593c720
PyroPeter <abi1789@googlemail.com>
2011-12-14 12:40:52 +0100

    vector.h: created (bug #11)

---
+++ src/vector.h
@@ -0,0 +1,30 @@
+/**
+ * @file
+ */
+
+#ifndef _MMB_VECTOR_H
+#define _MMB_VECTOR_H
+
+#include <math.h>
+
+/**
+ * 3D vector with int members x,y,z
+ */
+typedef struct Vector3i {
+	int x, y, z;
+} Vector3i;
+
+/**
+ * 2D vector with int members x,y
+ */
+typedef struct Vector2i {
+	int x, y;
+} Vector3i;
+
+#define VEC3CMP(a,o,b) (a.x o b.x && a.y o b.y && a.z o b.z)
+#define VEC2CMP(a,o,b) (a.x o b.x && a.y o b.y)
+
+#define VEC3IOP(a,o,b) ((Vector3i){a.x o b.x, a.y o b.y, a.z o b.z})
+#define VEC2IOP(a,o,b) ((Vector2i){a.x o b.x, a.y o b.y})
+
+#endif /* _MMB_VECTOR_H */
